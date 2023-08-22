ac05dd21bdf3dd1bc9c5be9e36fb99b0537f6712
Artem <artem.egorov.art2006@gmail.com>
2023-08-22 18:08:23 +0300

    создал DTO для продукта и убрал лишние using

--- Core/Domain/CreateProductCommandHandler.cs
+++ Core/Domain/CreateProductCommandHandler.cs
@@ -1,4 +1,3 @@
-using System.Security.Cryptography.X509Certificates;
 using Core.obj;
 using MediatR;
 
---
+++ Core/Domain/ProductDTO.cs
@@ -0,0 +1,9 @@
+namespace Core.Domain
+{
+    public class ProductDTO
+    {
+        public string Name { get; set; }
+        public int Coust { get; set; }
+        public string Key { get; set; } 
+    }
+}
