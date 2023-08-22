b7bc3413b7acd38fa6eb599f4bf7c4433079d209
Artem <artem.egorov.art2006@gmail.com>
2023-08-22 18:05:49 +0300

    хранилище создаётся с начальным значением

--- Core/Program.cs
+++ Core/Program.cs
@@ -1,3 +1,5 @@
+using Core;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<Store>();
 builder.Services.AddMediatR(cfg => {
     cfg.RegisterServicesFromAssembly(typeof(Program).Assembly);
 });
--- Core/Store.cs
+++ Core/Store.cs
@@ -5,6 +5,19 @@ namespace Core
 {
     public class Store
     {
+        public Store()
+        {
+            Products = new List<Product>()
+            {
+                new Product(){
+                    Id = Guid.NewGuid(),
+                    Name = "Product 1",
+                    Coust = 100,
+                    Key = "Key 1"
+                }
+            };
+        }
+
         public List<Product> Products { get; set; }
     }
 
