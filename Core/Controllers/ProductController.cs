1ef63770c6d2b9257787b2dd5c01442922f86508
Artem <artem.egorov.art2006@gmail.com>
2023-08-23 12:46:52 +0300

    Создал контроллер Продукта

---
+++ Core/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+using AutoMapper;
+using Core.Domain;
+using Core.obj;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+        ProductController(IMapper mapper, IMediator mediator)
+        {
+            _mapper = mapper;
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _mediator.Send(new ProductListQuery()));
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] ProductDTO product)
+        {
+            var command = _mapper.Map<CreateProductCommand>(product);
+            return Ok(await _mediator.Send(command));
+        }
+    }
+}
